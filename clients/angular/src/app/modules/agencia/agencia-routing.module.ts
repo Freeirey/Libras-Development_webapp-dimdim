6d4dfc334ab394391c7d23a79dd93ecf86011370
Freeirey <64961755+Freeirey@users.noreply.github.com>
2021-09-07 16:52:33 -0300

    Update agencia-routing.module.ts

--- clients/angular/src/app/modules/agencia/agencia-routing.module.ts
+++ clients/angular/src/app/modules/agencia/agencia-routing.module.ts
@@ -20,4 +20,4 @@ const routes: Routes = [
     imports: [CommonModule, RouterModule.forChild(routes)],
     exports: [RouterModule]
 })
-export class AgenciaRoutingModule { 
+export class AgenciaRoutingModule { }
