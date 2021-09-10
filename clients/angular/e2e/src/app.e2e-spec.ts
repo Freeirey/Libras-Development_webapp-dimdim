f6ad68271e7d7b025c5d78f15f03d436aa64ccf6
Freeirey <64961755+Freeirey@users.noreply.github.com>
2021-09-10 09:56:50 -0300

    Update app.e2e-spec.ts

--- clients/angular/e2e/src/app.e2e-spec.ts
+++ clients/angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bankflix-ng!');
+    expect(page.getTitleText()).toEqual('Welcome to bankDimDim-ng!');
   });
 
   afterEach(async () => {
